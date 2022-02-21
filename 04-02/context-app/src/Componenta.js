c18072121ebaa67be55f00ad585b710e476b99b0
jhsjah <98693755+jhsjah@users.noreply.github.com>
2022-02-21 22:32:29 +0530

    Add files via upload

---
+++ 04-02/context-app/src/Componenta.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Componentb from './Componentb';
+
+function Componenta() {
+  return <div>
+      <Componentb></Componentb>
+  </div>;
+}
+
+export default Componenta;
---
+++ 04-02/context-app/src/Componentb.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Componentc from './Componentc';
+
+function Componentb() {
+  return <div>
+      <Componentc></Componentc>
+  </div>;
+}
+
+export default Componentb;
